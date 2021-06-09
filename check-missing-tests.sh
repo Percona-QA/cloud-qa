9d5638e6914222953b535b962733d509a8621de5
Tomislav Plavcic <tomislav.plavcic@percona.com>
2021-06-09 10:56:31 +0200

    Add check-missing-tests.sh for listing differences between Jenkinsfile in operator repo and different provider pipelines

---
+++ check-missing-tests.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+operator=$1
+jenkinsfile=$2
+cloud="openshift eks lke gke minikube"
+tmp_file1=/tmp/check-missing-tests-1.log
+tmp_file2=/tmp/check-missing-tests-2.log
+
+function usage() {
+  echo "Usage: $(basename $0) <psmdb|pxc|pgo> <full_path_to_jenkinsfile>"
+}
+
+function extract_tests() {
+  local file=$1
+
+  cat ${file} | grep "runTest('" | sed "s/ //g" | sed "s/runTest('//" | sed "s/','.*')$//" | sed "s/')$//" | sort
+}
+
+if [[ $# -ne 2 ]]; then
+  echo "Illegal number of arguments!"
+  usage
+  exit 1
+fi
+
+if [[ ! -f ${jenkinsfile} ]]; then
+  echo "Jenkinsfile does't exist!"
+  exit 1
+fi
+
+# parse main jenkinsfile first
+extract_tests ${jenkinsfile} > ${tmp_file2}
+echo "###############"
+
+# check difference for each pipeline
+for c in ${cloud}; do
+  for f in ${operator}*_${c}*.groovy; do
+    echo "FILENAME: ${f}"
+    extract_tests ${f} > ${tmp_file1}
+    diff ${tmp_file1} ${tmp_file2} --side-by-side --suppress-common-lines | sed 's/\t//g' | sed 's/ //g' | sed 's/>/  > /'
+    if [[ ${PIPESTATUS[0]} -eq 0 ]]; then
+      echo "  No difference!"
+    fi
+    echo "###############"
+  done
+done
+
+rm -f ${tmp_file1}
+rm -f ${tmp_file2}
