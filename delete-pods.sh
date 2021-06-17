583f4cf74f166d106c5cf300c4b91665828f7825
Tomislav Plavcic <tomislav.plavcic@percona.com>
2021-06-17 10:07:04 +0200

    Minor update to delete-pods.sh

--- delete-pods.sh
+++ delete-pods.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-set -x
+#!/usr/bin/env bash
+
+echo "Deleting pods: $@ in a loop, press CTRL-C to stop."
+
 while true; do
-  kubectl delete pod $@ --force --grace-period=0
+  kubectl delete pod $@ --force --grace-period=0 >/dev/null 2>&1
   sleep 1
 done
