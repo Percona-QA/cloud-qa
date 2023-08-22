db74e5cd3ea4924f2b2c279b4d0ada0f7d9bf2f0
Tomislav Plavcic <tomislav.plavcic@percona.com>
2023-08-22 11:29:11 +0200

    Add kuttl-remove-test.sh

--- kuttl-remove-test.sh
+++ kuttl-remove-test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-# This script inserts a blank space into kuttl test list
+# This script descreases the numbers of kuttl test cases
 # (id's of test cases including and after requested will be increased)
 
 # Check if argument is provided
 if [ -z "$1" ]
   then
-    echo "Please provide a number of test case which number should be increased."
+    echo "Please provide a number of test case which number should be decreased."
     exit 1
 fi
 
-# Loop through the files in descending order and rename them
-for file in $(ls -v -r [0-9][0-9]-*)
+# Loop through the files and rename them
+for file in $(ls -v [0-9][0-9]-*)
 do
   # Get the number from the beginning of the filename
   number=$(echo "$file" | cut -d '-' -f 1)
@@ -19,8 +19,8 @@ do
   # Check if the number is greater than or equal to the argument
   if [ "$number" -ge "$1" ]
     then
-      # Increase the number by 1 and rename the file
-      new_number=$(expr "$number" + 1)
+      # Decrease the number by 1 and rename the file
+      new_number=$(expr "$number" - 1)
       new_number_padded=$(printf "%02d" "$new_number")
       new_file=$(echo "$file" | sed "s/^$number-/$new_number_padded-/")
 	  echo "moving: $file to: $new_file"
